371b3c2df97bd41b6bd1f0dd463199176bfbb3f1
Ashley Barr <lessuseless@duck.com>
2024-11-11 15:43:44 -0800

    initial commit

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,27 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "visual-studio-code"
+
+  # Communication Tools
+  "discord"
+  "notion"
+  "slack"
+  "telegram"
+  "zoom"
+
+  # Utility Tools
+  "syncthing"
+  #"1password@nightly"
+  
+  # Entertainment Tools
+  "vlc"
+
+  # Productivity Tools
+  "raycast"
+
+  # Browsers
+  "google-chrome"
+]

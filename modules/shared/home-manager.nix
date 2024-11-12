365a4122aa0534fba5072eed9b4943b2a3048aec
Ashley Barr <lessuseless@duck.com>
2024-11-12 11:58:34 -0800

    update casks

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -66,16 +66,16 @@ let name = "Ashley Barr";
       # Fabric AI #
       #############
 
-      # Loop through all files in the ~/.config/fabric/patterns directory
+      ## Loop through all files in the ~/.config/fabric/patterns directory
       for pattern_file in $HOME/.config/fabric/patterns/*; do
       
-      # Get the base name of the file (i.e., remove the directory path)
+      ## Get the base name of the file (i.e., remove the directory path)
       pattern_name=$(basename "$pattern_file")
   
-      # Create an alias in the form: alias pattern_name="fabric --pattern pattern_name"
+      ## Create an alias in the form: alias pattern_name="fabric --pattern pattern_name"
       alias_command="alias $pattern_name='fabric --pattern $pattern_name'"
   
-      # Evaluate the alias command to add it to the current shell
+      ## Evaluate the alias command to add it to the current shell
       eval "$alias_command"
       done
 

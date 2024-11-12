c807e38f512f521fae1c713b64c49cf76a7c99b0
Ashley Barr <lessuseless@anubis.tailec0ce0.ts.net>
2024-11-12 03:34:45 -0700

    add fabric aliases

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -61,6 +61,28 @@ let name = "Ashley Barr";
 
       # Always color ls and group directories
       alias ls='ls --color=auto'
+
+      #############
+      # Fabric AI #
+      #############
+
+      # Loop through all files in the ~/.config/fabric/patterns directory
+      for pattern_file in $HOME/.config/fabric/patterns/*; do
+      
+      # Get the base name of the file (i.e., remove the directory path)
+      pattern_name=$(basename "$pattern_file")
+  
+      # Create an alias in the form: alias pattern_name="fabric --pattern pattern_name"
+      alias_command="alias $pattern_name='fabric --pattern $pattern_name'"
+  
+      # Evaluate the alias command to add it to the current shell
+      eval "$alias_command"
+      done
+
+      yt() {
+          local video_link="$1"
+          fabric -y "$video_link" --transcript
+      }
     '';
   };
 

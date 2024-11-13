e52889a7781e5ae06128b2af7901df7daae94262
Ashley Barr <lessuseless@duck.com>
2024-11-13 15:56:52 -0700

    add package

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -1,8 +1,8 @@
-{ pkgs }:
+{ pkgs, ... }:
 
 with pkgs;
 let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
 shared-packages ++ [
   dockutil
-  pkgs.rnix-lsp # nix language server
+  rnix-lsp # nix language server
 ]

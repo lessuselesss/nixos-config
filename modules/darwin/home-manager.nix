16c5dd1f2675b3da0bfa589f04441db29e830324
Ashley Barr <lessuseless@duck.com>
2024-11-12 16:44:46 -0800

    add admin

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -19,76 +19,34 @@ in
   # Single User Setup #
   #####################
 
-  # ... Itza meee!
+  # First, set up the admin user
+  users.users.admin = {
+    name = "admin";
+    home = "/Users/admin";
+    shell = pkgs.zsh;  # or pkgs.bashInteractive if you prefer
+  };
+
+  # Then set up your standard user
   users.users.${user} = { 
     name = "${user}";
     home = "/Users/${user}";
-  #  hashedPassword = "$6$ewILqVwo85fq2Dtq$BXkOTy2hcBdgZ5gu7tOwd1Ns35oYHcUz/962YIF0FeDsbBTKQL8Xs73PAxMaF6nWHEJZVXhPKf.n/K7F.iGRx0";
     isHidden = false;
-    shell = pkgs.zsh; # | pkgs.bashInteractive; 
+    shell = pkgs.zsh;
   };
 
-  ################################
-  # Admin /w Standard User Setup #
-  ################################
-  
-  # # Define the "admin" user with root-like privileges
-  #users.users.admin = {
-  #  isNormalUser = true;
-  #  home = "/home/admin";
-  #  extraGroups = [ "wheel" ]; # wheel group allows sudo access
-  #  hashedPassword = "<hashed-password>"; # You can set a hashed password here or use `passwordFile` or `password` directly for simplicity
-  #  shell = pkgs.bashInteractive; # You can specify any shell here
-  #};
-
-  # # Define a less privileged user, "standard"
-  # users.users.standard = {
-  #   isNormalUser = true;
-  #   home = "/home/standard";
-  #   hashedPassword = "<hashed-password>";
-  #   shell = pkgs.bashInteractive;
-  # };
-
-
-  # # Ensure sudo privileges for members of the "wheel" group
-  # security.sudo = {
-  #   enable = true;
-  #   wheelNeedsPassword = true; # Members of the wheel group must provide a password for sudo
-  # };
-
-  # # Setup user packages, programs, and nix settings
-  # nix = {
-  #   package = pkgs.nix;
-  #   configureBuildUsers = true;
-
-  #   settings = {
-  #     trusted-users = [ "@admin" "root" "standard" ];
-  #     substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
-  #     trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-  #   };
-
-  #   gc = {
-  #     user = "root";
-  #     automatic = true;
-  #     interval = { Weekday = 0; Hour = 2; Minute = 0; };
-  #     options = "--delete-older-than 30d";
-  #   };
-
-
-  homebrew = {
-    enable = true;
-    casks = pkgs.callPackage ./casks.nix {};
-    # onActivation.cleanup = "uninstall";
-
-    masApps = {
-      #"1password" = 1333542190;
-      #"wireguard" = 1451685025;
-    };
-  };
-
-  # Enable home-manager
+  # Configure home-manager for both users
   home-manager = {
     useGlobalPkgs = true;
+    users.admin = { pkgs, config, lib, ... }: {
+      home = {
+        enableNixpkgsReleaseCheck = false;
+        packages = pkgs.callPackage ./packages.nix {};
+        stateVersion = "24.05";
+      };
+      programs = {} // import ../shared/home-manager.nix { inherit config pkgs lib; };
+      manual.manpages.enable = false;
+    };
+    
     users.${user} = { pkgs, config, lib, ... }: {
       home = {
         enableNixpkgsReleaseCheck = false;
@@ -101,10 +59,27 @@ in
         stateVersion = "24.05";
       };
       programs = {} // import ../shared/home-manager.nix { inherit config pkgs lib; };
-
-      # Marked broken Oct 20, 2022 check later to remove this
-      # https://github.com/nix-community/home-manager/issues/3344
       manual.manpages.enable = false;
+    };
+  };
+
+  # Configure Nix settings to allow admin user to manage the system
+  nix = {
+    settings = {
+      trusted-users = [ "@admin" "root" ];  # Give admin group Nix management capabilities
+      # ... other nix settings ...
+    };
+    # ... rest of nix configuration ...
+  };
+
+  homebrew = {
+    enable = true;
+    casks = pkgs.callPackage ./casks.nix {};
+    # onActivation.cleanup = "uninstall";
+
+    masApps = {
+      #"1password" = 1333542190;
+      #"wireguard" = 1451685025;
     };
   };
 

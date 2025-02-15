67d8ce25260db6c9e7890dd0f5526c7edc439b4b
Ashley Barr <lessuseless@duck.com>
2025-02-15 01:27:27 -0600

    fix fabric

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -82,17 +82,13 @@ in {
       #############
 
       ## Loop through all files in the ~/.config/fabric/patterns directory
-      for pattern_file in $HOME/.config/fabric/patterns/*; do
-
-      ## Get the base name of the file (i.e., remove the directory path)
-      pattern_name=$(basename "$pattern_file")
-
-      ## Create an alias in the form: alias pattern_name="fabric --pattern pattern_name"
-      alias_command="alias $pattern_name='fabric --pattern $pattern_name'"
-
-      ## Evaluate the alias command to add it to the current shell
-      eval "$alias_command"
-      done
+      if [ -d "$HOME/.config/fabric/patterns" ] && [ "$(ls -A $HOME/.config/fabric/patterns 2>/dev/null)" ]; then
+        for pattern_file in "$HOME/.config/fabric/patterns/"*; do
+          pattern_name=$(basename "$pattern_file")
+          alias_command="alias $pattern_name='fabric --pattern $pattern_name'"
+          eval "$alias_command"
+        done
+      fi
 
       yt() {
           local video_link="$1"

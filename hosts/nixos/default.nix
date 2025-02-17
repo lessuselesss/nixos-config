a66c706ebd838fc059cb7a2ce89ad753a4f00f00
Ashley Barr <lessuseless@duck.com>
2025-02-16 22:04:24 -0600

    fix fonts

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -308,15 +308,15 @@ in {
     ];
   };
 
-  fonts.packages = with pkgs; [
-    #dejavu_fonts
-    emacs-all-the-icons-fonts
-    feather-font # from overlay
-    jetbrains-mono
-    font-awesome
-    noto-fonts
-    noto-fonts-emoji
-  ];
+  #fonts.packages = with pkgs; [
+  #  dejavu_fonts
+  #  emacs-all-the-icons-fonts
+  #  feather-font # from overlay
+  #  jetbrains-mono
+  #  font-awesome
+  #  noto-fonts
+  #  noto-fonts-emoji
+  #];
 
   environment.systemPackages = with pkgs; [
     agenix.packages."${pkgs.system}".default # "x86_64-linux"

a10cbd471dffc8ca27dfc42d516d838871cbe8f7
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-02-24 22:17:29 -0600

    Update default.nix

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -63,16 +63,16 @@ in {
       ];
     };
 
-    gc = {
-      user = "root";
-      automatic = true;
-      interval = {
-        Weekday = 0;
-        Hour = 2;
-        Minute = 0;
-      };
-      options = "--delete-older-than 30d";
-    };
+#    gc = {
+#      user = "root";
+#      automatic = true;
+#      interval = {
+#       Weekday = 0;
+#       Hour = 2;
+#       Minute = 0;
+#      };
+#      options = "--delete-older-than 30d";
+#    };
 
     extraOptions = ''
       experimental-features = nix-command flakes
